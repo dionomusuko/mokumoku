d67c543351fc7af5dc509dcbe1acf1f604381768
dionomusuko <ruby.school114@gmail.com>
2020-08-05 21:47:48 +0900

    add header

---
+++ components/theHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-card color="grey lighten-4">
+    <v-toolbar extended>
+      <v-toolbar-title>ワイのポートフォリオ</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-home</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-github</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-twitter</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'theHeader'
+}
+</script>
+
+<style></style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,26 @@
 <template>
   <div>
+    <the-header />
     <nuxt />
   </div>
 </template>
+
+<script>
+import theHeader from '@/components/theHeader'
+
+export default {
+  components: {
+    theHeader
+  }
+}
+</script>
 
 <style>
 html {
   font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
     Roboto, 'Helvetica Neue', Arial, sans-serif;
+}
+body {
+  margin-top: 0;
 }
 </style>
