05534c181e37f71a89062ff8e7f0ef59be9585d6
dionomusuko <ruby.school114@gmail.com>
2020-08-05 11:34:01 +0900

    add vuetify

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,21 +1,21 @@
 module.exports = {
-  mode: "spa",
+  mode: 'spa',
   /*
    ** Headers of the page
    */
   head: {
-    title: "sample",
+    title: 'sample',
     meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "Nuxt.js project" }
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
   /*
    ** Customize the progress bar color
    */
-  loading: { color: "#3B8070" },
+  loading: { color: '#3B8070' },
   /*
    ** Build configuration
    */
@@ -26,12 +26,14 @@ module.exports = {
     extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
-          enforce: "pre",
+          enforce: 'pre',
           test: /\.(js|vue)$/,
-          loader: "eslint-loader",
+          loader: 'eslint-loader',
           exclude: /(node_modules)/
-        });
+        })
       }
     }
-  }
-};
+  },
+
+  buildModules: ['@nuxtjs/vuetify']
+}
